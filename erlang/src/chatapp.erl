f72cd9b2781cd3339e375f670bee6a1614561b8e
Rudolf Schlatte <rudi@Rudolfs-Mac-mini.local>
2020-05-06 22:17:48 +0200

    add command-line parser

--- erlang/src/chatapp.erl
+++ erlang/src/chatapp.erl
@@ -9,7 +9,25 @@
 
 %% escript Entry point
 main(Args) ->
-    io:format("Args: ~p~n", [Args]),
+    OptSpecList =
+        [
+         {iterations, $r, "iterations", {integer, 32}, "Number of iterations to execute"},
+         {clients, $c, "clients", {integer, 1024}, "Number of clients"},
+         {directories, $d, "directories", {integer, 8}, "Number of directories"},
+         {turns, $t, "turns", {integer, 32}, "Number of turns"},
+         {compute, $m, "compute", {integer, 55}, "Compute behavior probability"},
+         {post, $p, "post", {integer, 25}, "Post behavior probability"},
+         {leave, $l, "leave", {integer, 10}, "Leave behavior probability"},
+         {invite, $i, "invite", {integer, 10}, "Invite behavior probability"},
+         {befriend, $b, "befriend", {integer, 10}, "Probability of befriending"},
+         {parseable, $s, "parseable", {boolean, false}, "Generate parseable output"}
+        ],
+    case Parsed=getopt:parse(OptSpecList, Args) of
+        {ok, {Options, []}} ->
+            io:format("Args: ~w~n", [Parsed]);
+        _ ->
+            getopt:usage(OptSpecList, "chatapp")
+    end,
     erlang:halt(0).
 
 %%====================================================================
