a07097c74ef242586c8c7b284ad9d4c7ba02cb9b
Sebastian Blessing <sebastian.blessing@gmail.com>
2020-05-07 16:43:31 +0200

    adjusted caf runner

--- runners/caf.py
+++ runners/caf.py
@@ -1,7 +1,7 @@
 from runners.output_parser import OutputParser
 
 def setup(oBenchmarkRunner, cores, memory):
-  oBenchmarkRunner.configure("chatapp", "caf/build/bin", memory, args = ["--scheduler.max-threads=" + str(cores), "--parseable"])
+  oBenchmarkRunner.configure("caf", "caf/build/bin", memory, args = ["--scheduler.max-threads=" + str(cores), "--parseable"])
 
 def gnuplot(cores, files, results):
   OutputParser(files).parse(cores, results) 
