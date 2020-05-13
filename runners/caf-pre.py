84da752e489664c798d2097a0361a071fbb409a0
Joseph Noir <joseph.noir@posteo.net>
2020-05-13 22:57:37 +0200

    Shorten name, exclude executable cmake status file

--- runners/caf-pre.py
+++ runners/caf-pre.py
@@ -1,7 +1,7 @@
 from runners.output_parser import OutputParser
 
 def setup(oBenchmarkRunner, cores, phys_cores, scenario, memory):
-  oBenchmarkRunner.configure("caf-prerelease", "caf-prerelease/build", memory, args = ["--parseable", "--scheduler.max-threads=" + str(cores)] + scenario)
+  oBenchmarkRunner.configure("caf-pre", "caf-pre/build", memory, args = ["--parseable", "--scheduler.max-threads=" + str(cores)] + scenario, exclude = ['config.status'])
 
 def gnuplot(cores, files, results):
   OutputParser(files).parse(cores, results)
