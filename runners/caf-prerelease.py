44f27dabb04e0f5784285fe6853837b49c7b5009
Joseph Noir <joseph.noir@posteo.net>
2020-05-13 19:40:56 +0200

    Reneame caf-head to caf-prerelease

--- runners/caf-prerelease.py
+++ runners/caf-prerelease.py
@@ -1,7 +1,7 @@
 from runners.output_parser import OutputParser
 
 def setup(oBenchmarkRunner, cores, phys_cores, scenario, memory):
-  oBenchmarkRunner.configure("caf-head", "caf-head/build", memory, args = ["--parseable", "--scheduler.max-threads=" + str(cores)] + scenario)
+  oBenchmarkRunner.configure("caf-prerelease", "caf-prerelease/build", memory, args = ["--parseable", "--scheduler.max-threads=" + str(cores)] + scenario)
 
 def gnuplot(cores, files, results):
   OutputParser(files).parse(cores, results)
